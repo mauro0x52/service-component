8df37cc22b43ecfe170d8025c33f6bc8fdbf752f
Mauro Ribeiro <mauro0x52@gmail.com>
2015-07-17 08:55:32 -0400

    Adding properties

--- src/artifacts.js
+++ src/artifacts.js
@@ -17,6 +17,7 @@ var Artifacts = function (filePath, data) {
                 name : 'unknown' + parseInt(Math.random()*10000),
                 host : null,
                 port : null,
+                properties : {},
                 services : {},
                 components : {}
             }
@@ -31,5 +32,14 @@ Artifacts.prototype.getComponent = function (componentName) {
 Artifacts.prototype.save = function (serviceName) {
     return fs.writeFileSync(this.filePath, JSON.stringify(this.data, null, 4));
 };
+
+Artifacts.prototype.setProperty = function (name, value) {
+    if (this.data.hasOwnAttribute('properties') == false) = this.data.properties = {};
+    this.data.properties[name] = value;
+}
+
+Artifacts.prototype.getProperty = function (name) {
+    return this.data.properties && this.data.properties[name] ? this.data.properties.name : null;
+}
 
 module.exports = Artifacts;
